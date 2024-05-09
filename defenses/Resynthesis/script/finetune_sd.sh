9078443c3aed48afdc4007c834e37485820d2d32
Sifat Muhammad Abdullah <sifat@macbooks-MacBook-Pro.local>
2024-05-09 19:22:48 -0400

    adding resynthesis

---
+++ defenses/Resynthesis/script/finetune_sd.sh
@@ -0,0 +1,41 @@
+GPU_ID=${1}
+
+INIT_LR=1e-2
+EPOCH=150
+BATCH_SIZE=32
+INPUT_CHANNEL=512
+
+LR_RECONSTRUCTION=1e-3
+LOSS_WEIGHT=1
+
+RESUME='./pretrained_models/stylegan_celeba_stage5_noising/cls.pth.tar'
+SR_RESUME='./pretrained_models/stylegan_celeba_stage5_noising/sr.pth.tar'
+
+OUTPUT_PATH='<savemodel_path>'
+
+DATA_ROOT_POS='<path to real>'
+DATA_ROOT_NEG='<path to fake>'
+
+
+python train.py -a resnet50 \
+  --gpu ${GPU_ID} \
+  --batch-size ${BATCH_SIZE} \
+  --lr ${INIT_LR} \
+  --epochs ${EPOCH} \
+  --data-root-pos ${DATA_ROOT_POS} \
+  --data-root-neg ${DATA_ROOT_NEG} \
+  --input-channel ${INPUT_CHANNEL} \
+  --resume ${RESUME} \
+  --sr-weights-file ${SR_RESUME} \
+  --output-path ${OUTPUT_PATH} \
+  --lr-sr ${LR_RECONSTRUCTION} \
+  --lw-sr ${LOSS_WEIGHT} \
+  --no_dilation \
+  --sr-scale 4 \
+  --sr-num-features 64 \
+  --sr-growth-rate 64 \
+  --sr-num-blocks 16 \
+  --sr-num-layers 8 \
+  --idx-stages 5 \
+  --mode-sr denoising
+
